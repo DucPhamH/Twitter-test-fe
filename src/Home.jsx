df1343836afbcdbcb5af1f32c3cd0664a441edbb
DucPhamH <ducphamhong2@gmail.com>
2023-08-24 11:02:32 +0700

    streaming video

--- src/Home.jsx
+++ src/Home.jsx
@@ -40,6 +40,18 @@ export default function Home() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </span>
       </div>
+      <video controls width={500}>
+        <source
+          src="http://localhost:4000/static/video/2ce3e32755a1bc9a8ae1c8800.mp4"
+          type="video/mp4"
+        />
+      </video>
+      <video controls width={500}>
+        <source
+          src="http://localhost:4000/static/video-stream/2ce3e32755a1bc9a8ae1c8800.mp4"
+          type="video/mp4"
+        />
+      </video>
       <h1>Google OAuth 2.0</h1>
       <p className="read-the-docs">
         {isAuthenticated ? (
