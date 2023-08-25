01fd9b6fb870d25db8e00cd83cd536d67347163c
DucPhamH <ducphamhong2@gmail.com>
2023-08-25 09:44:55 +0700

    test hls

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,6 +2,15 @@ import { Link } from "react-router-dom";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import "vidstack/styles/defaults.css";
+import "vidstack/styles/community-skin/video.css";
+
+import {
+  MediaCommunitySkin,
+  MediaOutlet,
+  MediaPlayer,
+  MediaPoster,
+} from "@vidstack/react";
 
 console.log(import.meta.env);
 const getGoogleAuthUrl = () => {
@@ -40,9 +49,9 @@ export default function Home() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </span>
       </div>
-      <video controls width={500}>
+      {/* <video controls width={500}>
         <source
-          src="http://localhost:4000/static/video/2ce3e32755a1bc9a8ae1c8800.mp4"
+          src="http://localhost:4000/static/video/5bbcdee626956a25ca11ddf00.mp4"
           type="video/mp4"
         />
       </video>
@@ -51,7 +60,34 @@ export default function Home() {
           src="http://localhost:4000/static/video-stream/2ce3e32755a1bc9a8ae1c8800.mp4"
           type="video/mp4"
         />
-      </video>
+      </video> */}
+      <MediaPlayer
+        title="Sprite Fight"
+        src="http://localhost:4000/static/video-hls/eJf6Ha9QS1OYG_dNxpwax/master.m3u8"
+        // poster="https://image.mux.com/VZtzUzGRv02OhRnZCxcNg49OilvolTqdnFLEqBsTwaxU/thumbnail.webp?time=268&width=980"
+        // thumbnails="https://media-files.vidstack.io/sprite-fight/thumbnails.vtt"
+        aspectRatio={16 / 9}
+        crossorigin=""
+      >
+        <MediaOutlet>
+          <MediaPoster alt="Girl walks into sprite gnomes around her friend on a campfire in danger!" />
+          <track
+            src="https://media-files.vidstack.io/sprite-fight/subs/english.vtt"
+            label="English"
+            srcLang="en-US"
+            kind="subtitles"
+            default
+          />
+          <track
+            src="https://media-files.vidstack.io/sprite-fight/chapters.vtt"
+            srcLang="en-US"
+            kind="chapters"
+            default
+          />
+        </MediaOutlet>
+        <MediaCommunitySkin />
+      </MediaPlayer>
+
       <h1>Google OAuth 2.0</h1>
       <p className="read-the-docs">
         {isAuthenticated ? (
